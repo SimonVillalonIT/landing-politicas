0d7299051de9782bf4c52c9cd0a4a5794061f2a1
SimonVillalonIT <simonvillalon10@gmail.com>
2024-10-25 16:41:02 -0300

    Upload test

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Instituto de Politicas Publicas';
+export const SITE_DESCRIPTION = 'Estudiando politicas publicas';
---
+++ src/content/blog/Disertacion-acerca-del-tamano-de-la-nariz-de-Agusjkdev.md
@@ -0,0 +1,6 @@
+---
+title: Disertacion acerca del tamano de la nariz de Agusjkdev
+pubDate: 2024-10-18T03:00:00.000Z
+---
+
+**Hola**
